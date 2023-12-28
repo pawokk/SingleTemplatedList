6bed38759e17888b3f463c6bf4908e6b69f7a481
pawokk <pavel21324@yandex.ru>
2023-12-28 17:18:55 +0300

    переделал под реквесты

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "list.h"
 int main()
 {
-    list <int> list1;
+    Deck <int> list1;
     list1.pushBack(5);
     list1.pushBack(7);
     std::cout<<list1.tostring();
-    list <std::string> list2;
+    Deck <std::string> list2;
     list2.pushBack("qw");
     list2.pushBack("wr");
     std::cout<<list2.tostring();
+    std::cout<<list2.peekBack();
+    std::cout<<list2.peekFront();
 }
